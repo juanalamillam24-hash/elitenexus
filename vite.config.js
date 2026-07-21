92c287f0ee2ea9af1efd64ef5940ed022798638d
juanalamillam24-hash <juanalamillam24@gmail.com>
2026-07-21 18:28:49 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Si publicas en GitHub Pages en https://USUARIO.github.io/NOMBRE-REPO/
-// cambia base por "/NOMBRE-REPO/". Para Vercel o Netlify, deja "/".
+// base debe ser el nombre del repositorio entre barras, para GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/elitenexus/",
 });
